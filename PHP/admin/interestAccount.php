5d9d163d7eba584b49c5b27f2569ef6768c5940a
Utsav Parajuli <utsav.parajuli18@gmail.com>
2025-03-05 14:08:09 +0545

    Add: A page to display the interest rates of different types of member accounts

---
+++ PHP/admin/interestAccount.php
@@ -0,0 +1,43 @@
+<?php
+    $dbconfigs = include('../config.db.php');
+    $conn = mysqli_connect($dbconfigs->hostname, $dbconfigs->username, $dbconfigs->password, $dbconfigs->database);
+    if(!$conn){
+     die("Connection failed: ".mysqli_connect_errno());
+    }
+    $sql="SELECT DISTINCT account_type,interest_rate FROM accounts";
+    $result=mysqli_query($conn,$sql);
+    if(mysqli_num_rows($result)>0){
+        echo "<button class='editInt'><a href='editInterestAccount.php'>Edit Interest Rate</a></button>";
+        echo "<table border='1px'>";
+        echo "<tr>";
+        echo "<th>Account Type</th>
+        <th>Interest Rate</th>";
+        echo "</tr>";
+        while($row=mysqli_fetch_assoc($result)){
+            echo "<tr>";
+            echo "<td>".$row['account_type']."</td>";
+            echo "<td>".$row['interest_rate']."</td>";
+            echo "</tr>";
+        }
+        echo "</table>";     
+    }
+mysqli_close($conn);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../CSS/table.css">
+    <style type="text/css">
+        .editInt{
+            margin:0 20px 20px 0;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+<body>
+    <!-- <button class="aButton"><a href="createAccount.php" class="first">Create a new Account</a></button> -->
+</body>
+</html>
